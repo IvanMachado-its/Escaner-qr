a11e8abea9ee40b9b2cd42ce4df9e134d1844cec
IvanMachado-its <64422033+IvanMachado-its@users.noreply.github.com>
2024-09-27 10:21:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const path = require('path'); // Necesario para manejar rutas
+const path = require('path');
 const app = express();
 const PORT = 8080;
 
-// Función para cargar los códigos QR desde el archivo
 function loadQRCodes() {
     const data = fs.readFileSync('qrcodes.json', 'utf8');
     return JSON.parse(data);
 }
 
-// Función para guardar los códigos QR en el archivo
 function saveQRCodes(qrCodes) {
     fs.writeFileSync('qrcodes.json', JSON.stringify(qrCodes, null, 2));
 }
 
-app.use(bodyParser.json());
+app.get('/qrcodes', (req, res) => {
+    const qrCodes = loadQRCodes();
+    res.json(qrCodes);
+});
 
-// Sirve los archivos estáticos de la carpeta "public"
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para validar el código QR
 app.post('/validate', (req, res) => {
     const { qr } = req.body;
     let qrCodes = loadQRCodes();
     const qrCode = qrCodes.find(qrCode => qrCode.code === qr);
+app.get('/qrcodes', (req, res) => {
+    const qrCodes = loadQRCodes();
+    res.json(qrCodes);
+});
 
     if (qrCode) {
         if (!qrCode.used) {
-            qrCode.used = true; // Marcar como usado
-            saveQRCodes(qrCodes); // Guardar los cambios
+            qrCode.used = true;
+            saveQRCodes(qrCodes);
             res.json({ message: 'QR válido y registrado como usado' });
         } else {
             res.json({ message: 'El QR ya fue utilizado' });
@@ -40,7 +44,7 @@ app.post('/validate', (req, res) => {
     }
 });
 
-// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
